57345b8812d1f16a277403e6fa53052b8a8a6f36
ameliathefishy <162359310+hydrogencitrus@users.noreply.github.com>
2025-09-10 16:13:36 +0100

    meow
    hiss

---
+++ main.js
@@ -0,0 +1,22 @@
+function checkInput() {
+    let input = document.getElementById("input").value;
+    let num = 0;
+    let emdee5 = "";
+    let run = true;
+    while (run) {
+        num++;
+        console.log(input+num);
+        emdee5 = md5(input+num);
+        console.log(emdee5);
+        if (emdee5.charAt(0)+emdee5.charAt(1)+emdee5.charAt(2)+emdee5.charAt(3)+emdee5.charAt(4) == "00000") {
+            run = false;
+        }
+    }
+    result = num;
+    document.getElementById("output").innerHTML = result;
+}
+
+function copy() {
+    navigator.clipboard.writeText(document.getElementById("output").innerHTML);
+    alert("copied! ^_^")
+}
